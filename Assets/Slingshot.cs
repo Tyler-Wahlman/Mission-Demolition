bae606eb220d694ef43e1cd3b44ed72bf424d72b
Tyler Wahlman <76186495+Tyler-Wahlman@users.noreply.github.com>
2026-03-10 12:36:25 -0500

    Mission-Demolition
    Without Rubberband

---
+++ Assets/Slingshot.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public GameObject projectilePrefab;
+
+    [Header("Dynamic")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive( false );
+        launchPos = launchPointTrans.position;
+    }
+    
+    void OnMouseEnter()
+    {
+        //print( "Slingshot:OnMouseEnter()" );
+        launchPoint.SetActive( true );
+    }
+
+    void OnMouseExit()
+    {
+        //print( "Slingshot:OnMouseExit()" );
+        launchPoint.SetActive( false );
+    }
+
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate( projectilePrefab ) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
